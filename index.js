570a90764d170bafec8518e2f9278403c866eca9
Joshua Leung <aligorith@gmail.com>
2016-02-07 00:25:25 +1300

    Second Version - State Management
    * Use different icons + tooltips when enabled vs disabled
    * Set the initial button state to match the prefs value
    * When opening new windows, the button state is forced to match the global state

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var buttons = require('sdk/ui/button/toggle');
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var windows = require("sdk/windows").browserWindows;
 
 var { get: get_pref, set: set_pref } = require("sdk/preferences/service");
 var { when: unload } = require("sdk/system/unload");
@@ -6,23 +7,68 @@ var { when: unload } = require("sdk/system/unload");
 var prop_name = "media.autoplay.enabled"
 var old_value = get_pref(prop_name);
 
-
-var button = buttons.ToggleButton({
-  id: "toggle-autoplay",
-  label: "Enable/Disable Autoplay",
+/* State: Autoplay Enabled */
+const AutoplayEnabledState = {
+  checked: true,
+  label: "Autoplay Enabled. Click to disable",
   icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
+  	"16" : "./enabled-16.png",
+  	"32" : "./enabled-32.png",
+  	"64" : "./enabled-64.png"
+  }
+}
+
+/* State: Autoplay Disabled */
+const AutoplayDisabledState = {
+  checked: false,
+  label: "Autoplay Disabled. Click to enable",
+  icon : {
+  	"16" : "./disabled-16.png",
+  	"32" : "./disabled-32.png",
+  	"64" : "./disabled-64.png"
+  }
+}
+
+/* Button Definition - Used for creating each new button instance on new windows */
+var button = ToggleButton({
+  id: "toggle-autoplay",
+  
+  /* Set initial state - This depends on the pre-addon state, and is needed to ensure the first window works.
+   * These values MUSt be set, so we set them from the defaults
+   */
   checked: old_value,
-  onChange: function(state) {
-  	set_pref(prop_name, state.checked);
+  label:  (old_value) ? AutoplayEnabledState.label : AutoplayDisabledState.label,
+  icon:   (old_value) ? AutoplayEnabledState.icon : AutoplayDisabledState.icon,
+  
+  /* Invert the state when clicked - This is global, shared state... */
+  onClick: function(state) {
+  	if (get_pref(prop_name)) {
+  		button.state(button, AutoplayDisabledState);
+  		set_pref(prop_name, false);
+  	}
+  	else {
+  		button.state(button, AutoplayEnabledState);
+  		set_pref(prop_name, true);
+  	}
   }
 });
 
 
-// By AMO policy global preferences must be changed back to their original value
+/* Newly created windows will create local button state,
+ * so we need to force the button to use the correct global
+ * state
+ */
+windows.on("open", function(window) {
+	if (get_pref(prop_name)) {
+		button.state(button, AutoplayEnabledState)
+	}
+	else {
+		button.state(button, AutoplayDisabledState)	
+	}
+})
+
+
+/* By AMO policy global preferences must be changed back to their original value */
 unload(function() {
   set_pref(prop_name, old_value);
 });
